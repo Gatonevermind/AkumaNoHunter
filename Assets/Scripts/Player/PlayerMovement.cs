ad84bb140351f38eae0614c77aa6600ef11fa452
Kevin Gato Maroto <kevingatomaroto@gmail.com>
2014-11-30 23:39:04 +0100

    Alpha 0.5.5

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,9 +13,12 @@ public class PlayerMovement : MonoBehaviour
 	public float dashTimer;
 	public float coolDown;
 	public float dash = 10.0F;
+    public float active = 0;
 	
 	public bool combat;
-	
+
+    bool GodMode = false;
+
 	private Animator animator;
 	
 	private Vector3 objectiveDirection;
@@ -33,169 +36,222 @@ public class PlayerMovement : MonoBehaviour
 	
 	void Update()
 	{
-		
-		CharacterController controller = gameObject.GetComponent<CharacterController>();
-		
-		//moveDirection = transform.position - Camera.main.transform.position;
-		//moveDirection.Normalize();
-		//transform.rotation = moveDirection;
-		
-		// Calculates the module of the speed
-		float root = Mathf.Sqrt(speed * speed / 2);
-		
-		
-		// Acceleration and deceleration
-		interpolateDirection = new Vector3(Mathf.Lerp(interpolateDirection.x, objectiveDirection.x, acceleration),
-		                                   objectiveDirection.y,
-		                                   Mathf.Lerp(interpolateDirection.z, objectiveDirection.z, acceleration));
-		
-		// Calculates the direction
-		HorizontalMovement(speed, root);
-		
-		if (dashTimer > 0)
-			dashTimer -= Time.deltaTime;
-		if (dashTimer <= 0)
-		{
-			animator.SetBool ("Dash", false);
-			animator.SetBool ("DashBack", false);
-			dashTimer = 0;
-		}
-		
-		//desenvaine
-		if (Input.GetKeyDown(KeyCode.Q))
-		{
-			combat = !combat;
-		}
-		
-		if (combat)
-		{
-			animator.SetBool ("Combat", true);
-		}
-		else if (!combat)
-		{
-			animator.SetBool ("Combat", false);
-		}
-		//sprint
-		if (Input.GetKey(KeyCode.LeftShift))
-		{
-			animator.SetBool ("Sprint", true);
-			
-			if (Input.GetKey(KeyCode.W))
-				if (speed < sprint)
-			{
-				speed++;
-			}
-			else if (speed >= sprint)
-			{
-				speed = sprint;
-			}
-			
-			if ((Input.GetKey(KeyCode.W)) && (Input.GetKey(KeyCode.A)))
-				if (speed < sprintLateral)
-			{
-				speed++;
-			}
-			else if (speed >= sprintLateral)
-			{
-				speed = sprintLateral;
-			}
-			
-			
-			if ((Input.GetKey(KeyCode.W)) && (Input.GetKey(KeyCode.D)))
-				if (speed < sprintLateral)
-			{
-				speed++;
-			}
-			else if (speed >= sprintLateral)
-			{
-				speed = sprintLateral;
-			}
-			
-			
-			
-		}
-		else 
-		{
-			animator.SetBool("Sprint", false);
-			speed = 3;
-		}
-		/*
-        if(Input.GetKey(KeyCode.S))
+
+        if (Input.GetKeyUp(KeyCode.Alpha9))
+        {
+            if (GodMode == true)
             {
- 				speed = back;
+                GodMode = false;
+                active = 0;
             }
-                
-         else if (Input.GetKey(KeyCode.D))
+            else if (GodMode == false)
+            {
+                GodMode = true;
+                active = 1;
+            }
+        }
+
+        if (GodMode == false)
+        {
+            CharacterController controller = gameObject.GetComponent<CharacterController>();
+
+            //moveDirection = transform.position - Camera.main.transform.position;
+            //moveDirection.Normalize();
+            //transform.rotation = moveDirection;
+
+            // Calculates the module of the speed
+            float root = Mathf.Sqrt(speed * speed / 2);
+
+
+            // Acceleration and deceleration
+            interpolateDirection = new Vector3(Mathf.Lerp(interpolateDirection.x, objectiveDirection.x, acceleration),
+                                               objectiveDirection.y,
+                                               Mathf.Lerp(interpolateDirection.z, objectiveDirection.z, acceleration));
+
+            // Calculates the direction
+            HorizontalMovement(speed, root);
+
+            if (dashTimer > 0)
+                dashTimer -= Time.deltaTime;
+            if (dashTimer <= 0)
+            {
+                animator.SetBool("Dash", false);
+                animator.SetBool("DashBack", false);
+                dashTimer = 0;
+            }
+
+            //desenvaine
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                combat = !combat;
+            }
+
+            if (combat)
+            {
+                animator.SetBool("Combat", true);
+            }
+            else if (!combat)
+            {
+                animator.SetBool("Combat", false);
+            }
+            //sprint
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                animator.SetBool("Sprint", true);
+
+                if (Input.GetKey(KeyCode.W))
+                    if (speed < sprint)
+                    {
+                        speed++;
+                    }
+                    else if (speed >= sprint)
+                    {
+                        speed = sprint;
+                    }
+
+                if ((Input.GetKey(KeyCode.W)) && (Input.GetKey(KeyCode.A)))
+                    if (speed < sprintLateral)
+                    {
+                        speed++;
+                    }
+                    else if (speed >= sprintLateral)
+                    {
+                        speed = sprintLateral;
+                    }
+
+
+                if ((Input.GetKey(KeyCode.W)) && (Input.GetKey(KeyCode.D)))
+                    if (speed < sprintLateral)
+                    {
+                        speed++;
+                    }
+                    else if (speed >= sprintLateral)
+                    {
+                        speed = sprintLateral;
+                    }
+
+
+
+            }
+            else
+            {
+                animator.SetBool("Sprint", false);
+                speed = 3;
+            }
+            /*
+            if(Input.GetKey(KeyCode.S))
                 {
-                    if (speed > back)
+                    speed = back;
+                }
+                
+             else if (Input.GetKey(KeyCode.D))
                     {
-                        speed--;
-                    }
-                    else if (speed <= back)
+                        if (speed > back)
+                        {
+                            speed--;
+                        }
+                        else if (speed <= back)
+                        {
+                            speed = back;
+                        }
+                    }*/
+
+
+            // Jump/Dash
+            if (controller.isGrounded)
+            {
+                animator.SetBool("Jump", false);
+
+                //jump
+                if (Input.GetKey(KeyCode.Space))
+                {
+
+                    animator.SetBool("Jump", true);
+
+                    objectiveDirection = new Vector3(objectiveDirection.x, jumpSpeed, objectiveDirection.z);
+
+
+                }
+
+                //dash
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    if ((Input.GetKey(KeyCode.C)) && (dashTimer == 0))
                     {
-                        speed = back;
+
+                        objectiveDirection = new Vector3((objectiveDirection.x) * 10.6f, 0, (objectiveDirection.z) * 10.6f);
+                        dashTimer = coolDown;
                     }
-                }*/
-		
-		
-		// Jump/Dash
-		if (controller.isGrounded)
-		{
-			animator.SetBool ("Jump", false);
-			
-			//jump
-			if (Input.GetKey(KeyCode.Space))
-			{
-				
-				animator.SetBool ("Jump", true);
-				
-				objectiveDirection = new Vector3(objectiveDirection.x, jumpSpeed, objectiveDirection.z);
-				
-				
-			}
-			
-			//dash
-			if(Input.GetKey(KeyCode.LeftShift))
-			{
-				if ((Input.GetKey (KeyCode.C)) && (dashTimer == 0))
-				{
-					
-					objectiveDirection = new Vector3((objectiveDirection.x)*10.6f, 0, (objectiveDirection.z)*10.6f);
-					dashTimer = coolDown;
-				}
-			}
-			else
-			{
-				if (dashTimer == 0)
-				{
-					
-					if ((Input.GetKey (KeyCode.C)) && (Input.GetKey (KeyCode.W)))
-					{
-						animator.SetBool ("Dash",true);
-						objectiveDirection = new Vector3((objectiveDirection.x)*10.6f, 0, (objectiveDirection.z)*10.6f);
-						dashTimer = coolDown;
-					}
-					else if ((Input.GetKey (KeyCode.C)) && (Input.GetKey (KeyCode.S)))
-					{
-						animator.SetBool ("DashBack",true);
-						objectiveDirection = new Vector3((objectiveDirection.x)*20, 0, (objectiveDirection.z)*20);
-						dashTimer = coolDown;
-					}
-				}
-			}
-			
-		}
-		else
-		{	
-			animator.SetBool ("Run", false);
-			
-			objectiveDirection += new Vector3(objectiveDirection.x, -gravity * 1.5f,objectiveDirection.z) * Time.deltaTime;
-			
-		}
-		
-		
-		
-		controller.Move(interpolateDirection * Time.deltaTime);
+                }
+                else
+                {
+                    if (dashTimer == 0)
+                    {
+
+                        if ((Input.GetKey(KeyCode.C)) && (Input.GetKey(KeyCode.W)))
+                        {
+                            animator.SetBool("Dash", true);
+                            objectiveDirection = new Vector3((objectiveDirection.x) * 10.6f, 0, (objectiveDirection.z) * 10.6f);
+                            dashTimer = coolDown;
+                        }
+                        else if ((Input.GetKey(KeyCode.C)) && (Input.GetKey(KeyCode.S)))
+                        {
+                            animator.SetBool("DashBack", true);
+                            objectiveDirection = new Vector3((objectiveDirection.x) * 20, 0, (objectiveDirection.z) * 20);
+                            dashTimer = coolDown;
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                animator.SetBool("Run", false);
+
+                objectiveDirection += new Vector3(objectiveDirection.x, -gravity * 1.5f, objectiveDirection.z) * Time.deltaTime;
+
+            }
+
+
+
+            controller.Move(interpolateDirection * Time.deltaTime);
+        }
+
+        if (GodMode == true)
+        {
+            CharacterController controller = gameObject.GetComponent<CharacterController>();
+
+            // Calculates the module of the speed
+            float root = Mathf.Sqrt(speed * speed / 2);
+
+
+            // Acceleration and deceleration
+            interpolateDirection = new Vector3(Mathf.Lerp(interpolateDirection.x, objectiveDirection.x, acceleration),
+                                                objectiveDirection.y,
+                                                Mathf.Lerp(interpolateDirection.z, objectiveDirection.z, acceleration));
+
+            // Calculates the direction
+            HorizontalMovement(speed, root);
+
+
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                objectiveDirection = new Vector3(objectiveDirection.x, speed, objectiveDirection.z);
+            }
+            else if (Input.GetKey(KeyCode.LeftShift))
+            {
+                objectiveDirection = new Vector3(objectiveDirection.x, -speed, objectiveDirection.z); ;
+            }
+            else
+            {
+                objectiveDirection = new Vector3(objectiveDirection.x, 0, objectiveDirection.z);
+            }
+            //objectiveDirection += new Vector3(objectiveDirection.x, -gravity * 1.5f, objectiveDirection.z) * Time.deltaTime;
+
+            controller.Move(interpolateDirection * Time.deltaTime);
+
+        }
 	}
 	
 	
